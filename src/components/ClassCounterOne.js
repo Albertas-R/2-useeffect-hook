80416f9eb74b66a591689e4117dcef7c68750240
Albertas <albertas.rinkevicius@gmail.com>
2022-05-04 16:00:36 +0300

    Conditionally run effects

--- src/components/ClassCounterOne.js
+++ src/components/ClassCounterOne.js
@@ -1,3 +1,6 @@
+// useEffect after render
+// Conditionally run effects
+
 import React, { Component } from "react";
 
 class ClassCounterOne extends Component {
@@ -6,6 +9,7 @@ class ClassCounterOne extends Component {
 
     this.state = {
       count: 0,
+      name: "",
     };
   }
 
@@ -14,12 +18,20 @@ class ClassCounterOne extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    document.title = `Clicked ${this.state.count} times`;
+    if (prevState.count !== this.state.count) {
+      console.log("Updating document title");
+      document.title = `Clicked ${this.state.count} times`;
+    }
   }
 
   render() {
     return (
       <div>
+        <input
+          type="text"
+          value={this.state.name}
+          onChange={(e) => this.setState({ name: e.target.value })}
+        />
         <button onClick={() => this.setState({ count: this.state.count + 1 })}>
           Click {this.state.count} times
         </button>
--- src/components/HookCounterOne.js
+++ src/components/HookCounterOne.js
@@ -1,16 +1,20 @@
 // useEffect after render
+// Conditionally run effects
 
 import React, { useState, useEffect } from "react";
 
 function HookCounterOne() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
 
   useEffect(() => {
+    console.log("useEffect - Updating document title");
     document.title = `Clicked ${count} times`;
-  });
+  }, [count]);
 
   return (
     <div>
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
       <button onClick={() => setCount(count + 1)}>Click {count} times</button>
     </div>
   );
