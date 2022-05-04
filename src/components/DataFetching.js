ce3e9113f26754ad279c7c283e0b40d93c0a7d31
Albertas <albertas.rinkevicius@gmail.com>
2022-05-04 23:04:57 +0300

    Fetching data with useEffect (1 post)

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -1,28 +1,54 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+// Fetching data with useEffect (100 posts)
+// function DataFetching() {
+//   const [posts, setPosts] = useState([]);
+
+//   useEffect(() => {
+//     axios
+//       .get("https://jsonplaceholder.typicode.com/posts")
+//       .then((response) => {
+//         console.log(response);
+//         setPosts(response.data);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   }, []);
+
+//   return (
+//     <div>
+//       <ul>
+//         {posts.map((post) => (
+//           <li key={post.id}>{post.title}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// Fetching data with useEffect (1 post)
 function DataFetching() {
-  const [posts, setPosts] = useState([]);
+  const [post, setPost] = useState({});
+  const [id, setId] = useState(1);
 
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/posts")
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then((response) => {
         console.log(response);
-        setPosts(response.data);
+        setPost(response.data);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [id]);
 
   return (
     <div>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
+      <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <div>{post.title}</div>
     </div>
   );
 }
